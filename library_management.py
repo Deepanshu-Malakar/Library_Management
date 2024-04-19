885c7e0270519c8d9a0973e2d87a2ea7a5a84876
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2024-04-19 16:51:10 +0530

    Create library_management.py

---
+++ library_management.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self,title,author,year) -> None:
+        self.title=title
+        self.author=author
+        self.publication_year=year
+        self.copies_available=1
+        self.borrow_history=[]
+
+    def __str__(self) -> str:
+        return f"{self.title} published by {self.author} in year {self.publication_year}"
+    
+    def check_availability(self):
+        return self.copies_available>0
+    
+    def checkout(self,borrower):
+        if self.copies_available>0:
+            self.copies_available-=1
+            self.borrow_history.append(borrower)
+        else:
+            return f"no copies of {self.title} available :("
+        pass
+
+    def checkin(self):
+        self.copies_available+=1
+        pass
+    
+    
